608d257023b7d7efeb06a7e59aa573f0ae1778fb
AshiqTasdid <ashiqtasdid5@gmail.com>
2022-06-29 22:53:11 +0600

    Added Contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Head from 'next/head'
+import Header from '../components/Header/Header'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <div>
+      <Head>
+        <title>Contact Us - Ashiq The Dev</title>
+      </Head>
+      <Header/>
+      <Navbar/>
+      <Footer/>
+    </div>
   )
 }
 
