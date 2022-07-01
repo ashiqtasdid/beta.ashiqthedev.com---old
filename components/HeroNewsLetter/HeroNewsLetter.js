b0bad863bbed669d7a8020a07a04baf89e080e8f
AshiqTasdid <ashiqtasdid5@gmail.com>
2022-07-01 23:17:42 +0600

    Fixed ESlint errors

--- components/HeroNewsLetter/HeroNewsLetter.js
+++ components/HeroNewsLetter/HeroNewsLetter.js
@@ -9,23 +9,23 @@ const HeroNewsLetter = () => {
                     <p className="col-lg-10 fs-4">To get the latest updates, you have to sign up for our Newsletter. We will send you an email informing every updates. Just fill this form & you are good to go</p>
                 </div>
                 <div className='col-md-10 mx-auto col-lg-5' id="revue-embed">
-                    <form action="https://www.getrevue.co/profile/AshiqTheDev/add_subscriber" method="post" id="revue-form" name="revue-form" target="_blank" className='p-4 p-md-5 border rounded-3 bg-light'>
+                    <form action="https://www.getrevue.co/profile/AshiqTheDev/add_subscriber" method="post" id="revue-form" name="revue-form" target="_blank" rel="noreferrer   " className='p-4 p-md-5 border rounded-3 bg-light'>
                         <div className="form-floating mb-3">
                             <input className="form-control" placeholder="name@example.com" type="email" name="member[email]" id="member_email" />
-                            <label for="member_email">Email address</label>
+                            <label htmlFor="member_email">Email address</label>
                         </div>
                         <div className="form-floating mb-3">
                             <input className="form-control" placeholder="First name... (Optional)" type="text" name="member[first_name]" id="member_first_name" />
-                            <label for="member_first_name">First name <span className="optional">(Optional)</span></label>
+                            <label htmlFor="member_first_name">First name <span className="optional">(Optional)</span></label>
                         </div>
                         <div className="form-floating mb-3">
                             <input className="form-control" placeholder="Last name... (Optional)" type="text" name="member[last_name]" id="member_last_name" />
-                            <label for="member_last_name">Last name <span classNameName="optional">(Optional)</span></label>
+                            <label htmlFor="member_last_name">Last name <span classNameName="optional">(Optional)</span></label>
                         </div>
 
                         <input className='w-100 btn btn-lg btn-primary' type="submit" value="Subscribe" name="member[subscribe]" id="member_submit" />
                         <hr className="my-4" />
-                        <div className="text-muted">By subscribing, you agree with Ours & Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms of Service</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
+                        <div className="text-muted">By subscribing, you agree with Ours & Revue’s <a target="_blank" rel='noreferrer' href="https://www.getrevue.co/terms">Terms of Service</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
                     </form>
                 </div>
             </div>
