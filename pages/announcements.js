0189cee2028a7354b9a228a89153f43b8da54524
AshiqTasdid <ashiqtasdid5@gmail.com>
2022-07-09 16:29:08 +0600

    Added title

--- pages/announcements.js
+++ pages/announcements.js
@@ -3,10 +3,16 @@ import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Footer/Footer'
 import Newsletter from '../components/HeroNewsLetter/HeroNewsLetter'
 import Temptweets from '../components/Temptweets/Temptweets'
+import Header from '../components/Header/Header'
+import Head from 'next/head'
 
 const announcements = () => {
   return (
     <div>
+      <Header/>
+      <Head>
+        <title>Announcements - Ashiq The Dev</title>
+      </Head>
       <Navbar />
       <div></div>
       <Temptweets/>
