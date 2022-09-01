7a111ab11a8a90833c9905e22ce281f78c25fee8
AshiqTasdid <ashiqtasdid5@gmail.com>
2022-09-01 21:07:58 +0600

    Added A New announcement

--- components/Temptweets/Temptweets.js
+++ components/Temptweets/Temptweets.js
@@ -7,6 +7,11 @@ const Temptweets = () => {
             <div className="shadow p-3 mb-5 bg-white rounded card mt-5 mx-auto ml-auto mr-auto " style={{ width: 450 }} >
 
                 <blockquote className="twitter-tweet d-inline-flex"><p lang="en" dir="ltr">As you know, We are using Twitter Revue for our Newsletter backend. As of today, Twitter Revue is currently down. They&#39;re trying to fix the issue. So, our newsletter will be down until they fix it. <a href="https://t.co/L7GhBq1ETM">pic.twitter.com/L7GhBq1ETM</a></p>&mdash; Ashiq The Dev - Beta Updates (@AshiqTheDevBeta) <a href="https://twitter.com/AshiqTheDevBeta/status/1545653882692734976?ref_src=twsrc%5Etfw">July 9, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+
+            </div>
+            <div className="shadow p-3 mb-5 bg-white rounded card mt-5 mx-auto ml-auto mr-auto " style={{ width: 450 }} >
+
+                <blockquote className="twitter-tweet"><p lang="en" dir="ltr">You guys may be wondering, where we have been. The answer is we are making the base DB of our primary system. So, That&#39;s why we couldn&#39;t make updates to our beta website. We are currently working on our DB project.</p>&mdash; Ashiq The Dev - Beta Updates (@AshiqTheDevBeta) <a href="https://twitter.com/AshiqTheDevBeta/status/1565336443035082753?ref_src=twsrc%5Etfw">September 1, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
                 <div className="alert alert-success" role="alert">
                     If the tweet does not show up, please reload the page.
                 </div>
